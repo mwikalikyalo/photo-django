9f944efe55f159e38e79a088f3a82c1f8c0eeaf8
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-26 05:53:13 +0300

    Add the category class

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  
+  category_name = models.CharField(max_length=100)
+  category_slug = models.CharField(max_length=20)
+  category_description = models.CharField(max_length=100)

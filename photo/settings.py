c83f4275053979c37fff515bf4af4e79e29d396e
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-02 00:16:54 +0300

    Add readme.md

--- photo/settings.py
+++ photo/settings.py
@@ -80,6 +80,10 @@ WSGI_APPLICATION = 'photo.wsgi.application'
 
 
 # Database
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 # development
 if config('MODE')=="dev":
@@ -100,6 +104,7 @@ else:
             default=config('DATABASE_URL')
         )
     }
+
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
